a814da9490aa30c3df20ab2d9085639b60cd1cee
houwy <houwyvenny@126.com>
2018-12-09 21:22:04 +0800

    first commit

---
+++ app/src/main/java/cn/edu/pku/ss/huowy/shihou/MainActivity.java
@@ -0,0 +1,15 @@
+package cn.edu.pku.ss.huowy.shihou;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cn.edu.pku.ss.houwy.shihou.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+    }
+}
